a3af1f2fe01f5f9a3f9898db958924d75f5a5d54
ohnadir <nadirhossain336@gmail.com>
2025-09-25 15:27:41 +0600

    done

--- src/components/ui/web-pages/home/RotatingOrbit.tsx
+++ src/components/ui/web-pages/home/RotatingOrbit.tsx
@@ -9,32 +9,47 @@ const RotatingOrbit = () => {
         <div className="w-[343px] h-[343px] flex items-center justify-center relative">
             <motion.div
                 className="relative border border-[#D2D2D2] w-[343px] h-[343px] rounded-full flex items-center justify-center"
-                animate={{ rotate: 360 }}
-                transition={{ repeat: Infinity, duration: 9, ease: "linear" }}
+                animate={{ rotate: [0, 180, 360] }}
+                transition={{
+                    repeat: Infinity,
+                    duration: 9,
+                    ease: ["easeInOut", "linear", "easeInOut"],
+                    times: [0, 0.5, 1]
+                }}
             >
                 <div className="absolute top-0 left-1/2 -translate-x-1/2">
                     <div className="bg-[#F79549] border border-[#D2D2D2] h-4 w-4 rounded-full -mt-2" />
-                    <Image src={"/hrm.png"} alt='' height={80} width={80}  />
+                    <Image src={"/hrm.png"} alt='' height={80} width={80} />
                 </div>
 
                 <motion.div
                     className="relative border border-[#D2D2D2] w-[293px] h-[293px] rounded-full flex items-center justify-center"
-                    animate={{ rotate: 360 }}
-                    transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
+                    animate={{ rotate: [0, 180, 360] }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 12,
+                        ease: ["easeInOut", "linear", "easeInOut"],
+                        times: [0, 0.5, 1]
+                    }}
                 >
-                    <div className="absolute top-0 left-1/2 -translate-x-1/2">
+                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
                         <div className="bg-[#F79549] border border-[#D2D2D2] h-4 w-4 rounded-full -mt-2" />
-                        <Image src={"/erp.png"} alt='' height={80} width={80}  />
+                        <Image src={"/erp.png"} alt='' height={80} width={80} />
                     </div>
 
                     <motion.div
                         className="relative border border-[#D2D2D2] w-[230px] h-[230px] rounded-full flex items-center justify-center"
-                        animate={{ rotate: 360 }}
-                        transition={{ repeat: Infinity, duration: 12, ease: "linear" }}
+                        animate={{ rotate: [0, 180, 360] }}
+                        transition={{
+                            repeat: Infinity,
+                            duration: 15,
+                            ease: ["easeInOut", "linear", "easeInOut"],
+                            times: [0, 0.5, 1]
+                        }}
                     >
                         <div className="absolute top-0 left-1/2 -translate-x-1/2">
-                            <div className="bg-[#F79549] border border-[#D2D2D2] h-4 w-4 rounded-full -mt-2" />
-                            <Image src={"/pos.png"} alt='' height={80} width={80}  />
+                            <div className="bg-[#F79549] border border-[#D2D2D2] h-4 w-4 rounded-full -mt-3" />
+                            <Image src={"/pos.png"} alt='' height={80} width={80} />
                         </div>
 
                     </motion.div>
@@ -42,7 +57,7 @@ const RotatingOrbit = () => {
             </motion.div>
 
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                <Image src={"/productIcons.png"} alt='' height={110} width={80}  />
+                <Image src={"/productIcons.png"} alt='' height={110} width={80} />
             </div>
         </div>
 
