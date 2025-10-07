55702b459a0a099491e0b6f6a27354ad24f9d8c4
Nadir Hossain <nadirhossain336.com>
2025-10-07 22:51:24 +0600

    laptop screen responsive

--- src/components/ui/web-pages/home/BannerDetails.tsx
+++ src/components/ui/web-pages/home/BannerDetails.tsx
@@ -7,33 +7,29 @@ const BannerDetails = () => {
   return (
     <div className="  w-full z-10 bg-[#000000E5]/90 md:bg-gradient-to-r from-[#f8fcfc]  to-[#e0dfdf] md:-mt-[25px] py-[40px] md:py-[100px]">
       {/* <div className=" w-full h-full bg-black/60 "></div>  */}
-      <div className=" container flex flex-col md:flex-row items-center gap-6 md:gap-0 z-20  ">
-        <div className=" w-full z-20">
+      <div className=" container flex flex-col xl:flex-row items-center gap-36 lg:gap-6 md:gap-0 z-20  ">
+        <div className="md:h-[580px] w-[50%] 2xl:w-[650px] relative mx-auto xl:mx-0">
           <Image
             src={"/banner.png"}
             alt=""
-            height={600}
-            width={590}
-            className="rounded-lg md:h-[580px] md:w-[650px] object-cover"
+            fill
+            objectFit="cover"
           />
         </div>
 
-        <div className="w-full text-lg md:text-xl leading-10 tracking-wide text-[#B7B7B7]  md:text-gray-600 ">
+        <div className="w-[50%] text-lg md:text-xl lg:text-[18px] 2xl:text-xl leading-10 tracking-wide text-[#B7B7B7]  md:text-gray-600 ">
           <span className="text-primary text-[35px]"> Betopia </span> Limited
           began its journey in 2014 with a simple dream, to use technology to
           solve real business problems. In 2015, we saw early growth as more
           clients trusted our approach. By 2019, our name was reaching across
           borders and expansion had truly begun.
           <br className="hidden md:block" />
-          <br />
           The year 2020 tested everyone, but it also became our turning point,
           we scaled operations and helped businesses go digital when they needed
           it most.
           <br />
-          <br />
           In 2022, we took bold steps into new areas like AI, Cloud,
           Cybersecurity, and SaaS, shaping solutions for the future.
-          <br />
           <br />
           Today, our journey continues with pride,{" "}
           <span className="font-bold">
