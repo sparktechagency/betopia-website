f35272bf15fdc528012b974b897a2a189d7262f0
Yead191 <yead191@gmail.com>
2025-10-10 18:23:57 +0600

    career page md device responsive fixed

--- src/components/ui/web-pages/career/BetopiaVision.tsx
+++ src/components/ui/web-pages/career/BetopiaVision.tsx
@@ -14,33 +14,31 @@ const BetopiaVision: React.FC = () => {
         everyone can thrive and excel professionally.
       </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-[30px] mt-[52px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-[30px] mt-[52px] items-stretch">
         {BetopiaVisionData.map((data, index) => (
           <div
             key={index}
-            className={` ${
-              index === 0 ? " bg-[#FCD7BC]/50" : " bg-[#FEF4ED] "
-            }  h-[322.4px] rounded-[12px]`}
+            className={`${
+              index === 0 ? "bg-[#FCD7BC]/50" : "bg-[#FEF4ED]"
+            } rounded-[12px] flex flex-col justify-center h-full`}
           >
-            <div className="flex items-center justify-center h-full w-full px-[32px]">
-              <div>
-                <div className="h-[48px] overflow-hidden mx-auto flex-center w-[48px] mb-[15px] bg-white rounded-[8px] shadow-2xl">
-                  <Image
-                    style={{ borderRadius: "8px" }}
-                    src={data.img}
-                    alt="Dubai"
-                    width={60}
-                    height={60}
-                  />
-                </div>
-
-                <h2 className="text-[30px] font-semibold mb-[15px] text-center">
-                  {data.name}
-                </h2>
-                <p className="text-[18px] text-center text-[#797979] leading-[160%] font-normal">
-                  {data.description}
-                </p>
+            <div className="flex flex-col items-center justify-center text-center flex-grow px-[32px] py-6">
+              <div className="h-[48px] w-[48px] flex-center mb-[15px] bg-white rounded-[8px] shadow-2xl overflow-hidden">
+                <Image
+                  style={{ borderRadius: "8px" }}
+                  src={data.img}
+                  alt={data.name}
+                  width={60}
+                  height={60}
+                />
               </div>
+
+              <h2 className="text-[24px] md:text-[30px] font-semibold mb-[15px]">
+                {data.name}
+              </h2>
+              <p className="text-[16px] md:text-[18px] text-[#797979] leading-[160%] font-normal flex-grow">
+                {data.description}
+              </p>
             </div>
           </div>
         ))}
