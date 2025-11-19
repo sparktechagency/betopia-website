f77bb6ebd88b3674ef689eda5c37d2988405f4cb
Naziya Sultana Mithila <121658415+MithilaKhan@users.noreply.github.com>
2025-11-19 16:37:55 +0600

    update videos

--- src/datas/pages/about.tsx
+++ src/datas/pages/about.tsx
@@ -30,7 +30,7 @@ export const owners = [
 ]
 
 export const aboutBannerData = {
-    videoSrc: "https://res.cloudinary.com/dd0giqouc/video/upload/v1757499565/About_Us_Page_y7cpsp.mp4",
+    videoSrc: "https://res.cloudinary.com/ds1njqypu/video/upload/v1763546476/About_Us_Page_vj6jlz.mp4",
     title: <p><span className="text-white"> From </span> <span className="text-primary"> Bangladesh </span > to the World</ p >,
     subtitle: "Shaping the Future of Enterprise Technology",
     description: "Betopia Limited is a global IT transformation company, delivering cloud, AI, cybersecurity, and compliance-first solutions that help enterprises innovate and scale with confidence.",
