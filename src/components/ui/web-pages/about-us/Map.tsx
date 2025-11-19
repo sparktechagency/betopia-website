21ccbac472396e42f6fb13b53dd0eb68cb069a71
Farhana Sharna <farhanasharna22@gmail.com>
2025-11-19 11:31:49 +0600

    Complete map section of about page

--- src/components/ui/web-pages/about-us/Map.tsx
+++ src/components/ui/web-pages/about-us/Map.tsx
@@ -6,133 +6,108 @@ import {
   Geographies,
   Geography,
   Marker,
-  ZoomableGroup,
 } from "react-simple-maps";
 import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 import { officeCards } from "@/datas/pages/about";
 
-type MapPosition = {
-  center: [number, number];
-  zoom: number;
-};
 
 const Map: React.FC = () => {
   const [index, setIndex] = useState(0);
-
-  const [position, setPosition] = useState<MapPosition>({
-     center: [0, 20],
-     zoom: 1,   
-  });
 
   // Move to Next Card
   const nextCard = () => {
     const newIndex = (index + 1) % officeCards.length;
     setIndex(newIndex);
-
-    const card = officeCards[newIndex];
-
-    // Move map to new location
-    setPosition({
-      center: [card.location.lng, card.location.lat],
-      zoom: 5,
-    });
   };
 
   // When clicking marker
   const handleMarkerClick = (card: any, i: number) => {
     setIndex(i);
-
-    setPosition({
-      center: [card.location.lng, card.location.lat],
-      zoom: 5,
-    });
   };
 
   const card = officeCards[index];
 
   return (
     <div className="bg-linear-to-r from-[#020019] via-[#14113f] via-70% to-[#201c61] h-full w-full p-4">
-    <div className="container h-[736px] relative overflow-hidden">
-  <ComposableMap
-    projection="geoMercator"
-    projectionConfig={{ scale: 107 }}
-    width={800}
-    height={600}
-    style={{ width: "100%", height: "100%" }}
-  >
-
-       
-            {/* WORLD SHAPES */}
-            <Geographies geography="/features.json">
-              {({ geographies }) =>
-                geographies.map((geo) => {
-                  const highlight = geo.id === "ARE" || geo.id === "PHL";
-                  return (
-                    <Geography
-                      key={geo.rsmKey}
-                      geography={geo}
-                      style={{
-                        default: {
-                          fill: highlight ? "#E63946" : "#1A1A1A",
-                          stroke: "#FFFFFF",
-                          strokeWidth: 0.2,
-                        },
-                        hover: { fill: highlight ? "#D62828" : "#333" },
-                        pressed: { fill: highlight ? "#B71C1C" : "#222" },
-                      }}
-                    />
-                  );
-                })
-              }
-            </Geographies>
-
-            {/* MARKERS */}
-            {officeCards.map((item, i) => {
-              const active = i === index;
-              return (
-                <Marker
-                  key={i}
-                  coordinates={[item.location.lng, item.location.lat]}
-                  onClick={() => handleMarkerClick(item, i)}
-                >
-                  {/* ACTIVE MARKER */}
-                  <g transform="translate(-12, -24)" cursor="pointer">
-                    <circle
-                      cx="12"
-                      cy="10"
-                      r={active ? 7 : 1}
-                      fill={active ? "#f99b4e" : "#FF5722"}
-                      stroke="#fff"
-                      strokeWidth="2"
-                    />
-                    <path
-                      d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z"
-                      fill={active ? "#f99b4e" : "#FF5722"}
-                      stroke="#fff"
-                      strokeWidth="2"
-                    />
-                  </g>
-
-                  {/* LABEL */}
-                  <text
-                    textAnchor="middle"
-                    y={-35}
+      <div className="container h-[650px] md:h-[736px] relative overflow-hidden">
+        <ComposableMap
+          projection="geoMercator"
+          projectionConfig={{ scale: 107 }}
+          width={800}
+          height={600}
+          style={{ width: "100%", height: "100%" }}
+        >
+          {/* map shape */}
+          <Geographies geography="/features.json">
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const highlight = geo.id === "ARE" || geo.id === "PHL";
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
                     style={{
-                      fontFamily: "system-ui",
-                      fill: "white",
-                      fontSize: active ? "14px" : "8px",
-                      fontWeight: active ? "bold" : "normal",
+                      default: {
+                        fill: highlight ? "#E63946" : "#1A1A1A",
+                        stroke: "#FFFFFF",
+                        strokeWidth: 0.2,
+                      },
+                      hover: { fill: highlight ? "#D62828" : "#333" },
+                      pressed: { fill: highlight ? "#B71C1C" : "#222" },
                     }}
-                  >
-                    {item.title}
-                  </text>
-                </Marker>
-              );
-            })}
+                  />
+                );
+              })
+            }
+          </Geographies>
+
+          {/* markers */}
+          {officeCards.map((item, i) => {
+            const active = i === index;
+            return (
+              <Marker
+                key={i}
+                coordinates={[item.location.lng, item.location.lat]}
+                onClick={() => handleMarkerClick(item, i)}
+              >
+                {/* active markers*/}
+                <g transform="translate(-12, -24)" cursor="pointer">
+                  <circle
+                    cx="12"
+                    cy="10"
+                    r={active ? 7 : 1}
+                    fill={active ? "#f99b4e" : "#FF5722"}
+                    stroke="#fff"
+                    strokeWidth="2"
+                  />
+                  <path
+                    d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z"
+                    fill={active ? "#f99b4e" : "#FF5722"}
+                    stroke="#fff"
+                    strokeWidth="2"
+                  />
+                </g>
+
+                {/* country name */}
+                <text
+                  textAnchor="middle"
+                  y={-35}
+                  style={{
+                    fontFamily: "system-ui",
+                    fill: "white",
+                    fontSize: active ? "14px" : "8px",
+                    fontWeight: active ? "bold" : "normal",
+                  }}
+                >
+                  {item.title}
+                </text>
+              </Marker>
+            );
+          })}
         </ComposableMap>
 
-        {/* CARD SECTION */}
+        {/* cards */}
         <div className="absolute top-[55%] left-0 h-full md:h-[300px]">
           <div className="bg-[#1C1F37A6]/65 backdrop-blur border border-white/20 shadow-2xl rounded-4xl p-6 lg:p-11 lg:w-[689px]">
             <div className="flex justify-between items-center">
